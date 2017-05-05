b74118afe481582f3a82de90d21ef521fa3e6fc7
Fausto Sanchez <fausto1124@gmail.com>
2017-05-04 21:22:45 -0300

    Web Api Project creation. Initial packages needed added. Unit of Work and Repository patern basics added.

---
+++ Product/Web-Api/VeniPorEl/Data.Repository/GenericRepository/Repository.cs
@@ -0,0 +1,74 @@
+﻿using Data.Access;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository.GenericRepository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public Context ContextRepo;
+        public DbSet<TEntity> dbSet;
+
+        public Repository(Context context)
+        {
+            ContextRepo = context;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public TEntity Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.Where(predicate).ToList();
+        }
+
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.SingleOrDefault(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            dbSet.AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            dbSet.Attach(entity);
+            ContextRepo.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Atach(TEntity entity)
+        {
+            dbSet.Attach(entity);
+        }
+    }
+}
---
+++ Product/Web-Api/VeniPorEl/Data.Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Data.Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context Context;
+
+        public UnitOfWork()
+        {
+            Context = new Context();
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
