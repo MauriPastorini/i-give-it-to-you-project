bcfed89349363e3052fbbd01ed90464a1bc55dd3
Fausto Sanchez <fausto1124@gmail.com>
2017-05-28 12:57:45 -0300

    User and roles defined

---
+++ Product/Web-Api/VeniPorEl/Data/AdminRole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class AdminRole : IRole
+    {
+        public int RoleId
+        {
+            get
+            {
+                return (int)User.Roles.Admin
+            }
+        }
+
+        public bool IsAdmin()
+        {
+            return true;
+        }
+
+        public bool IsNormalUser()
+        {
+            return false;
+        }
+    }
+}
